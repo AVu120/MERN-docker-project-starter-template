9e6481577c00d20c031a3e6f5209c0222da44ecf
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-01-11 16:13:59 +1100

    Initial commit.

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setState(res));
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <p>Hello world</p>
+      <p>{state}</p>
+    </div>
+  );
+}
+
+export default App;
